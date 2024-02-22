0302c7b7584edfdb9a17eac105ea6828c07247a3
3-yellow-1 <3.yellow.1@gmail.com>
2024-02-22 18:34:18 +0900

    [🔨Feat] Screen configuration after logging in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DashBoard from './Components/DashBoard';
+import Diarymain from './screen/diary/Diarymain.js';
 import DiaryEntry from './Components/DiaryEntry';
 import Login from './screen/login.js';
+import Adminmain from './screen/admin/Adminmain.js';
 
 function App() {
   return (
       <Routes>
-        <Route path="/calendar" element={<DashBoard />} />
+        <Route path="/calendar" element={<Diarymain />} />
         <Route path="/diary/:date" element={<DiaryEntry />} />
-        <Route path="/DashBoard" element={<DashBoard />} /> 
+        <Route path="/Diarymain" element={<Diarymain />} /> 
+        <Route path="/Adminmain" element={<Adminmain />} /> 
         <Route path="/" element={<Login />} /> 
       </Routes>
   );
---
+++ src/screen/admin/Adminmain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Adminmain() {
+  return (
+    <div className='Adminmain'>
+        <h2>
+          관리자 페이지
+        </h2>
+    </div>
+  );
+}
+
+export default Adminmain;
