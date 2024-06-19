e9523ffe2e570748da509dce0cf8eaf9dd2f8c38
6cessfuldev <6cessfuldev@gmail.com>
2024-06-19 17:58:14 +0900

    🛠️Fix: 중복 선언된 변수 삭제

--- src/service/string.js
+++ src/service/string.js
@@ -15,7 +15,6 @@ export const urlCcreateDiary = urlPrefix + "/svc/diary";
 export const urlMeDiaryList = urlPrefix + "/svc/me/diaryList";
 export const urlAddMembers = urlPrefix + "/pub/members ";
 export const urlComment = urlPrefix + "/pub/diary/{diaryId}/comment"
-export const urlAddMembers = urlPrefix + "/pub/members";
 
 // 어드민페이지
 export const urlMembers = urlPrefix + "/pub/members";
